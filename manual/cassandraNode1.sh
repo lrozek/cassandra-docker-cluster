8d035736c45a51a6c08f7e7210c355adcb9fb1e3
lrozek <lukasz.rozek@consileon.pl>
2020-08-12 14:51:26 +0200

    limiting memory taken

--- manual/cassandraNode1.sh
+++ manual/cassandraNode1.sh
@@ -1,1 +1,1 @@
-docker run --name cassandra-node1 -d cassandra:3.11.7
+docker run --name cassandra-node1 -e HEAP_NEWSIZE=1G -e MAX_HEAP_SIZE=1G -d cassandra:3.11.7
--- manual/cassandraNode2.sh
+++ manual/cassandraNode2.sh
@@ -1,2 +1,2 @@
 IP_NODE1="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' cassandra-node1)"
-docker run --name cassandra-node2 -d -e CASSANDRA_SEEDS="${IP_NODE1}" cassandra:3.11.7
+docker run --name cassandra-node2 -d -e CASSANDRA_SEEDS="${IP_NODE1}" -e HEAP_NEWSIZE=1G -e MAX_HEAP_SIZE=1G cassandra:3.11.7
